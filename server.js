53b22a26a291010d522b735e9c663f3c6d9b9f62
Qua-Qu <didi.aryadi2002@gmail.com>
2023-05-12 13:13:30 +0700

    client

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 import express from "express"
-import mysql from "mysql2"
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import cookieParser from "cookie-parser";
+import multer from "multer";
+
 const app = express()
 const port = 9090
 
-app.use(express.json());
 
-app.get('/', (req, res) => res.json('Hello World!'))
+app.use(express.json());
+app.use(cookieParser());
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post("/api/upload", upload.single("file"), function (req, res) {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
